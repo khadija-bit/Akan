ce36cf14ef0598a15626c9aa896bc04b920840f2
khadija <sumeyahassan34@gmail.com>
2020-02-16 18:53:21 +0300

    commit Akan

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 var day of the month = day of month()
+var getAge = function(year,month,date){
+    var today = new Date();
+    var dob = new Date();
+    dob.setFullYear(year);
+    dob.setMonth(month-1);
+    dob.setDate(date);
+    var timeDiff = today.valueOf() - dob.valueOf();
+    var milliInDay = 24*60*60*1000;
+    var noOfDays = timeDiff / milliInDay;
+    var daysInYear = 365.242;
+    return  ( noOfDays / daysInYear ) ;
+}
