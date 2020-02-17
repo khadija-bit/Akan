128887ae907627112da6856fe3996d6b65b43edc
khadija <sumeyahassan34@gmail.com>
2020-02-17 09:32:50 +0300

    Add commit Akan

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+var maleNames=[
+   "kwasi", "kwadwo", "kwabena", "kwaku", "yaw", "kofi", "kwame",]
+var femaleNames=[
+    "akosua", "adwoa", "abenaa", "akua", "yaa", "afua", "ama"]
+   function result{
+var century=pereseInt(document.getElementById("century").value);
+var year=pereseInt(document.getElementById("year").value);
+var month=pereseInt(document.getElementById("month").value);
+var day=pereseInt(document.getElementById("day").value);
+ifcentury(century<=18||century21> ) {
+    alert("Right input")
+    return "false"
+}
+ else if(year<=1800||year2100>)
+ {
+    alert("Right input")
+    return "false"
+}
+else if(month<=0||month12>)
+{
+    alert("Right input")
+    return "false"
+}
+else if(day<=0||day30>) 
+{
+    alert("Right input")
+    return "false"
+}
+if the maleNames.checked
